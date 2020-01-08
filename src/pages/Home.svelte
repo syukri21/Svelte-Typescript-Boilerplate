4ddd674aefaaed2b66d9192583ec8a7ad963fd3e
Syukri <syukrihsb148@gmail.com>
2020-01-08 15:25:59 +0700

    change some directory

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,64 +1,11 @@
 <script lang="ts">
-  import jQuery from "jquery";
-  import "slick-carousel/slick/slick.min.js";
-  import "../sass/slick-theme.scss";
-
-  import Icon from "fa-svelte";
-  import { faPlay } from "@fortawesome/free-solid-svg-icons/faPlay";
-
-  import { onMount } from "svelte";
-
-  onMount(() => {
-    jQuery("#slickCarousel").slick();
-  });
+  import Carousel from "./home/Carousel.svelte";
 </script>
-
-<style>
-  .container-carousel-item {
-    width: 80%;
-    height: 500px;
-  }
-
-  .height-fit {
-    height: 100%;
-  }
-
-  .max-width {
-    max-width: 450px;
-  }
-</style>
 
 <div class="hero">
   <div class="section py-0 ">
-    <div class="container has-background-primary">
-      <div id="slickCarousel">
-        <div class="container-carousel-item">
-          <div class="columns height-fit">
-            <div class="column height-fit" />
-            <div class="column height-fit d-flex align-items-center">
-              <div class="d-flex flex-column max-width">
-                <h5 class="is-size-5">Men Accessories</h5>
-                <h1 class="is-size-2">Always Be A Gentelman.</h1>
-                <p class="is-size-6 mt-1">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim,
-                  eos sed? Placeat nihil distinctio similique earum
-                  reprehenderit. Optio consectetur.
-                </p>
-                <div class="mt-4">
-                  <button class="button is-link is-light is-uppercase">
-                    <span class="icon">
-                      <Icon icon={faPlay} />
-                    </span>
-                    <span>Shop Now</span>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="container-carousel-item" />
-        <div class="container-carousel-item" />
-      </div>
+    <div class="container has-background-primary" >
+      <Carousel></Carousel>
     </div>
   </div>
 </div>
---
+++ src/pages/home/Carousel.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import jQuery from "jquery";
+  import "slick-carousel/slick/slick.min.js";
+  import "../sass/slick-theme.scss";
+
+  import Icon from "fa-svelte";
+  import { faPlay } from "@fortawesome/free-solid-svg-icons/faPlay";
+
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    jQuery("#slickCarousel").slick();
+  });
+</script>
+
+<style>
+  .container-carousel-item {
+    width: 80%;
+    height: 500px;
+  }
+
+  .height-fit {
+    height: 100%;
+  }
+
+  .max-width {
+    max-width: 450px;
+  }
+</style>
+
+<div id="slickCarousel">
+  <div class="container-carousel-item">
+    <div class="columns height-fit">
+      <div class="column height-fit">
+        <img src="./images/black-suit.png" alt="#" />
+      </div>
+      <div class="column height-fit d-flex align-items-center">
+        <div class="d-flex flex-column max-width">
+          <h5 class="is-size-5">Men Accessories</h5>
+          <h1 class="is-size-2">Always Be A Gentelman.</h1>
+          <p class="is-size-6 mt-1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, eos
+            sed? Placeat nihil distinctio similique earum reprehenderit. Optio
+            consectetur.
+          </p>
+          <div class="mt-4">
+            <button class="button is-link is-light is-uppercase">
+              <span class="icon">
+                <Icon icon={faPlay} />
+              </span>
+              <span>Shop Now</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container-carousel-item" />
+  <div class="container-carousel-item" />
+</div>
