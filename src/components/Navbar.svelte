4d7818f47f0829ae185b713968d317bc7b619c24
Syukri <syukrihsb148@gmail.com>
2020-01-08 16:50:37 +0700

    fix carousel svelte

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -25,17 +25,25 @@
     <div id="navbarBasicExample" class="navbar-menu">
 
       <div class="navbar-end">
-        <a class="navbar-item" href="/home" use:link>Home</a>
-        <a class="navbar-item" href="/documentation" use:link>Shop</a>
-        <a class="navbar-item" href="/documentation" use:link>Collections</a>
-        <a class="navbar-item" href="/documentation" use:link>Blog</a>
-        <a class="navbar-item" href="/documentation" use:link>Pages</a>
-
-        <div class="navbar-item has-dropdown is-hoverable is-primary">
+        <a class="navbar-item has-text-link" href="/home" use:link>Home</a>
+        <a class="navbar-item has-text-link" href="/documentation" use:link>
+          Shop
+        </a>
+        <a class="navbar-item has-text-link" href="/documentation" use:link>
+          Collections
+        </a>
+        <a class="navbar-item has-text-link" href="/documentation" use:link>
+          Blog
+        </a>
+        <a class="navbar-item has-text-link" href="/documentation" use:link>
+          Pages
+        </a>
+        <div
+          class="navbar-item has-text-link has-dropdown is-hoverable is-primary">
           <a class="navbar-link" href="/home" use:link>Elements</a>
           <div class="navbar-dropdown">
-            <a class="navbar-item" href="/home" use:link>Home</a>
-            <a class="navbar-item" href="/documentation" use:link>
+            <a class="navbar-item has-text-link" href="/home" use:link>Home</a>
+            <a class="navbar-item has-text-link" href="/documentation" use:link>
               Documentation
             </a>
           </div>
--- src/pages/home/Carousel.svelte
+++ src/pages/home/Carousel.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
   import jQuery from "jquery";
   import "slick-carousel/slick/slick.min.js";
-  import "../sass/slick-theme.scss";
+  import "../../sass/slick-theme.scss";
 
   import Icon from "fa-svelte";
   import { faPlay } from "@fortawesome/free-solid-svg-icons/faPlay";
+  import { faChevronLeft } from "@fortawesome/free-solid-svg-icons/faChevronLeft";
+  import { faChevronRight } from "@fortawesome/free-solid-svg-icons/faChevronRight";
 
   import { onMount } from "svelte";
 
   onMount(() => {
-    jQuery("#slickCarousel").slick();
+    jQuery("#slickCarousel").slick({
+      prevArrow: jQuery("#carouselLeft"),
+      nextArrow: jQuery("#carouselRight"),
+      dots: true,
+      dotsClass: "position-absolute slick-dots position-dot"
+    });
+    console.log("TCL: jQ", jQuery("#carouselLeft"));
   });
 </script>
 
@@ -26,35 +34,69 @@
   .max-width {
     max-width: 450px;
   }
+
+  .to-right-bottom {
+    bottom: 20px;
+    right: 20px;
+  }
+
+  .circle {
+    display: flex;
+    position: absolute;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+    height: 100%;
+    width: 100%;
+  }
+
+  .circle-container {
+    height: 450px;
+    width: 450px;
+    border-radius: 50%;
+  }
 </style>
 
-<div id="slickCarousel">
-  <div class="container-carousel-item">
-    <div class="columns height-fit">
-      <div class="column height-fit">
-        <img src="./images/black-suit.png" alt="#" />
-      </div>
-      <div class="column height-fit d-flex align-items-center">
-        <div class="d-flex flex-column max-width">
-          <h5 class="is-size-5">Men Accessories</h5>
-          <h1 class="is-size-2">Always Be A Gentelman.</h1>
-          <p class="is-size-6 mt-1">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, eos
-            sed? Placeat nihil distinctio similique earum reprehenderit. Optio
-            consectetur.
-          </p>
-          <div class="mt-4">
-            <button class="button is-link is-light is-uppercase">
-              <span class="icon">
-                <Icon icon={faPlay} />
-              </span>
-              <span>Shop Now</span>
-            </button>
+<div class="position-relative">
+  <div class="circle">
+    <div class="circle-container has-background-white" />
+  </div>
+  <div id="slickCarousel">
+    <div class="container-carousel-item">
+      <div class="columns height-fit">
+        <div class="column height-fit">
+          <img src="./images/black-suit.png" alt="#" />
+        </div>
+        <div class="column height-fit d-flex align-items-center">
+          <div class="d-flex flex-column max-width">
+            <h5 class="is-size-5">Men Accessories</h5>
+            <h1 class="is-size-1">Always Be A Gentelman.</h1>
+            <p class="is-size-6 mt-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, eos
+              sed? Placeat nihil distinctio similique earum reprehenderit. Optio
+              consectetur.
+            </p>
+            <div class="mt-4">
+              <button class="button is-link is-light is-uppercase">
+                <span class="icon">
+                  <Icon icon={faPlay} />
+                </span>
+                <span>Shop Now</span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
     </div>
+    <div class="container-carousel-item" />
+    <div class="container-carousel-item" />
   </div>
-  <div class="container-carousel-item" />
-  <div class="container-carousel-item" />
+  <div class="position-absolute to-right-bottom">
+    <button class="is-size-5 button is-priamry is-light" id="carouselLeft">
+      <Icon class=" has-text-link " icon={faChevronLeft} />
+    </button>
+    <button class="is-size-5 button is-orimary is-light" id="carouselRight">
+      <Icon class=" has-text-link" icon={faChevronRight} />
+    </button>
+  </div>
 </div>
